398e91cc224a270992f3fa3dbc570dbf6995b317
Gefy Fitry Wj <113435647+gefyfitrywj@users.noreply.github.com>
2022-10-18 11:35:38 +0700

    Add files via upload

---
+++ AplikasiToDoList.java
@@ -0,0 +1,229 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Gefy 
+ */
+public class AplikasiToDoList {
+
+    /**
+     * 
+     */
+    public static String [] model = new String [10];
+    
+    public static java.util.Scanner scanner = new java.util.Scanner (System.in);
+    
+    public static void main (String [] args) {
+     viewShowTodolist();  
+    }
+   
+    /**
+     *  Menampilkan todo list 
+     */
+    public static void showTodoList() {
+    System.out.println("TODOLIST");
+        for (int i = 0; i < model.length; i++) {
+        String todo = model[i];
+        int no = i + 1;
+
+    if (todo != null) {
+    System.out.println(no + ". " + todo);
+      }
+    }
+  }
+
+    public static void testShowTodoList() {
+    model[0] = "Belajar Java Dasar";
+    model[1] = "Studi Kasus Java Dasar : Aplikasi Todolist";
+    showTodoList();
+    }
+
+    /**
+     *  Menambah todo ke list
+     * @param todo
+     */
+    public static void addTodoList(String todo) {
+    // cek apakah model penuh?
+    boolean isFull = true;
+        for (String model1 : model) {
+            if (model1 == null) {
+                // model masih ada yang kosong
+                isFull = false;
+                break;
+            }
+        }
+
+    // jika penuh, kita resize ukuran array 2x lipat
+    if (isFull) {
+       String [] temp = model;
+       model = new String[model.length * 2];
+
+        System.arraycopy(temp,0, model, 0, temp.length);
+    }
+
+    // tambahkan ke posisi yang data array nya NULL
+    for (int i = 0; i < model.length; i++) {
+    if (model[i] == null) {
+        model[i] = todo;
+        break;
+      }
+    }
+  }
+
+    public static void testAddTodoList() {
+    for (int i = 0; i < 25; i++) {
+      addTodoList("Contoh Todo Ke." + i);
+    }
+
+    showTodoList();
+  }
+
+    /**
+     * Mehapus todo dari list
+     * @param number
+     * @return 
+     */
+    public static boolean removeTodoList(Integer number) {
+      if ((number - 1) >= model.length) {
+      return false;
+    } else if (model[number - 1] == null) {
+      return false;
+    } else {
+      for (int i = (number - 1); i < model.length; i++) {
+          if (i == (model.length - 1)) {
+          model[i] = null;
+        } else {
+          model[i] = model[i + 1];
+        }
+      }
+      return true;
+    }
+  }
+
+    public static void testRemoveTodoList() {
+    addTodoList("Satu");
+    addTodoList("Dua");
+    addTodoList("Tiga");
+    addTodoList("Empat");
+    addTodoList("Lima");
+
+    boolean result = removeTodoList(20);
+    System.out.println(result);
+
+    result = removeTodoList(7);
+    System.out.println(result);
+
+    result = removeTodoList(2);
+    System.out.println(result);
+
+    showTodoList();
+  }
+
+    public static String input(String info) {
+    System.out.print(info + " : ");
+    String data = scanner.nextLine();
+    return data;
+  }
+
+    public static void testInput() {
+    String name = input("Nama");
+    System.out.println("Hi " + name);
+
+    String channel = input("Channel");
+    System.out.println(channel);
+  }
+
+    /**
+     *  Menampilkan view todo list
+     */
+    public static void viewShowTodoList() {
+        OUTER:
+        while (true) {
+            showTodoList();
+            System.out.println("MENU : ");
+            System.out.println("1. Tambah");
+            System.out.println("2. Hapus");
+            System.out.println("x. Keluar");
+            String input = input("Pilih");
+            switch (input) {
+                case "1" -> viewAddTodoList();
+                case "2" -> viewRemoveTodoList();
+                case "x" -> {
+                    break OUTER;
+                }
+                default -> System.out.println("Pilihan tidak dimengerti");
+            }
+        }
+  }
+
+    public static void testViewShowTodoList() {
+    addTodoList("Satu");
+    addTodoList("Dua");
+    addTodoList("Tiga");
+    addTodoList("Empat");
+    addTodoList("Lima");
+    viewShowTodoList();
+  }
+
+  /**
+   *  Menampilkan view menambahkan todo list
+   */
+    public static void viewAddTodoList() {
+    System.out.println("MENAMBAH TODOLIST");
+
+    String todo = input("Todo (x Jika Batal)");
+
+    if (todo.equals("x")) {
+      // batal
+    } else {
+      addTodoList(todo);
+    }
+  }
+
+    public static void testViewAddTodoList() {
+    addTodoList("Satu");
+    addTodoList("Dua");
+
+    viewAddTodoList();
+
+    showTodoList();
+  }
+
+    /**
+     *  Menampilkan view menghapus todo list
+     */
+    public static void viewRemoveTodoList() {
+    System.out.println("MENGHAPUS TODOLIST");
+
+    String number = input("Nomor yang Dihapus (x Jika Batal)");
+
+    if (number.equals("x")) {
+      // batal
+    } else {
+      boolean success = removeTodoList(Integer.valueOf(number));
+      if (!success) {
+        System.out.println("Gagal menghapus todolist : " + number);
+      }
+    }
+  }
+
+    public static void testViewRemoveTodoList() {
+    addTodoList("Satu");
+    addTodoList("Dua");
+    addTodoList("Tiga");
+
+    showTodoList();
+
+    viewRemoveTodoList();
+
+    showTodoList();
+  }
+
+    private static void viewShowTodolist() {
+      throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
+
